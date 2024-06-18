9e85dcfa7f490313fab0415a05c475fdeb0d8776
Handukkk <johandvn05@gmail.com>
2024-06-18 23:35:42 +0700

    first commit

---
+++ scripts/click.js
@@ -0,0 +1,15 @@
+const first = document.querySelector('.first');
+const second = document.querySelector('.second');
+const third = document.querySelector('.third');
+
+first.addEventListener('click', () => {
+    console.log('aaaaa');
+});
+
+second.addEventListener('click', () => {
+    console.log('bbbbb');
+});
+
+third.addEventListener('click', () => {
+    console.log('ccccc');
+});
